2e4a998c4f825a07a18b2b3829aef569dac04d41
Andrew Napitupulu <angularsprings403@gmail.com>
2024-04-19 14:37:38 +0700

    belajar insert

--- database_test.go
+++ database_test.go
@@ -2,18 +2,19 @@ package belajar_golang_database
 
 import (
 	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
-	"testing"
+	"time"
 )
 
-func TestEmpty(t *testing.T) {
-
-}
-
-func TestOpenConnection(t *testing.T) {
+func GetConnection() *sql.DB {
 	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/belajar_golang_database")
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
+
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxLifetime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
+
+	return db
 }
